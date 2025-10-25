69f591550508afbd394fc9576ae153ca65b80fbb
viplavsoni <viplavsoni3333@gmail.com>
2025-10-25 22:13:11 +0530

    Two way data bindings

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,22 +1,28 @@
 import { useState } from "react"
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+    const [playerName, setPlayerName] = useState(initialName)
     const [isEditing, setIsEditing] = useState(false);
 
     function playerEditingHandler() {
         setIsEditing((editing) => !editing)
     }
 
-    let playerName = <span className="player-name">{name}</span>
+    function handleChange(event) {
+        console.log(event)
+        setPlayerName(event.target.value)
+    }
+
+    let player = <span className="player-name">{playerName}</span>
 
     if (isEditing) {
-        playerName = <input type="text" value={name} required />
+        player = <input type="text" value={playerName} onChange={handleChange} required />
     }
 
     return (
         <li>
             <span className="player-info">
-                {playerName}
+                {player}
                 <span className="player-symbol">{symbol}</span>
             </span>
             <button onClick={playerEditingHandler}>{isEditing ? 'Save' : 'Edit'}</button>
