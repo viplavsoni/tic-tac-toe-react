563cf9a2c6dcd6016a42770a2c874ed4dd9a9083
viplavsoni <viplavsoni3333@gmail.com>
2025-10-25 16:29:45 +0530

    player display & symbol

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,21 @@ function App() {
 
 
   return (
-    <h2>Coming Soon....</h2>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">Player 1</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">Player 2</span>
+            <span className="player-symbol">O</span>
+          </li>
+        </ol>
+        Game Board
+      </div>
+    </main>
   )
 }
 
